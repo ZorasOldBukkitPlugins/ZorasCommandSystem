1851262e7630d6f2b954eaf1ffda53868384baac
Zora <MrZoraman@Gmail.com>
2014-05-31 14:28:48 -0700

    We don't need this anymore!

--- ZorasCommandSystem/src/zorascommandsystem/test/TestCommand.java
+++ ZorasCommandSystem/src/zorascommandsystem/test/TestCommand.java
@@ -9,22 +9,13 @@ import zorascommandsystem.bukkitcompat.CSBukkitCommand;
 
 public class TestCommand implements CSBukkitCommand
 {
-	private final String id;
-	
-	public TestCommand(String id)
-	{
-		this.id = id;
-	}
-
 	@Override
 	public boolean execute(CommandSender sender, Player player, String cmdName, String[] preArgs, String[] args)
 	{
-		System.out.println("id: " + id);
 		System.out.println("cmdName: " + cmdName);
 		System.out.println("Command sent!");
 		System.out.println("preArgs: " + Arrays.asList(preArgs).toString());
 		System.out.println("args: " + Arrays.asList(args).toString());
 		return true;
 	}
-
 }
--- ZorasCommandSystem/src/zorascommandsystem/test/ZorasCommandSystem.java
+++ ZorasCommandSystem/src/zorascommandsystem/test/ZorasCommandSystem.java
@@ -11,9 +11,9 @@ public class ZorasCommandSystem extends JavaPlugin
 	{
 		BukkitCommandSystem cs = new BukkitCommandSystem(this);
 		
-		cs.registerCommand("some stuff", new TestCommand("some stuff"));
-		cs.registerCommand("maggots * {good|bad}", new TestCommand("maggots"));
-		cs.registerCommand("{snoo|pingas}", new TestCommand("snoopingas"));
+		cs.registerCommand("some stuff", new TestCommand());
+		cs.registerCommand("maggots * {good|bad}", new TestCommand());
+		cs.registerCommand("{snoo|pingas}", new TestCommand());
 	}
 	
 	@Override
