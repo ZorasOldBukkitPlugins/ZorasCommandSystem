3be28c271e3d09f1b29774c6508d6187210e5859
Zora <MrZoraman@Gmail.com>
2014-05-31 14:17:56 -0700

    better debug info

--- ZorasCommandSystem/src/zorascommandsystem/test/TestCommand.java
+++ ZorasCommandSystem/src/zorascommandsystem/test/TestCommand.java
@@ -9,10 +9,17 @@ import zorascommandsystem.bukkitcompat.CSBukkitCommand;
 
 public class TestCommand implements CSBukkitCommand
 {
+	private final String id;
+	
+	public TestCommand(String id)
+	{
+		this.id = id;
+	}
 
 	@Override
 	public boolean execute(CommandSender sender, Player player, String[] preArgs, String[] args)
 	{
+		System.out.println("id: " + id);
 		System.out.println("Command sent!");
 		System.out.println("preArgs: " + Arrays.asList(preArgs).toString());
 		System.out.println("args: " + Arrays.asList(args).toString());
